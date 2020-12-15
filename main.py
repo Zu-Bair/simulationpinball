402542d58000c9778d07f3705cf1b2963342c193
Zu-Bair <zubairc836@gmail.com>
2020-12-15 12:54:20 +0500

    final commit with implementation of rebound bumpers and score calculation

--- main.py
+++ main.py
@@ -1,63 +1,71 @@
-from flask import Flask
-app = Flask(__name__)
-
-
 def grid_size():
-    try:
-        global x, y, m, n, grid
-        m, n = [int(y) for y in input("Enter values of m and n : ").split()]
-        if 2 < m < 51 and 2 < n < 51:
-            grid = [[0] * n for _ in range(m)]
-            for item in grid:
-                print(item)
-        else:
-            print("Values are not in range please try again ...")
-            grid_size()
-    except:
-        print("Values of m or n is not correct please try again ...")
+    score, total = 0, 0
+    m, n = [int(y) for y in input("Enter values of m and n : ").split()]
+    if 2 < m < 51 and 2 < n < 51:
+        grid = [[0] * n for _ in range(m)]
+        for item in grid:
+            print(item)
+        bumpers_input(m, n, grid, score)
+    else:
+        print("Values are not in range please try again ...")
         grid_size()
 
 
-def bumpers_input():
-    global value, cost, x, y
+def bumpers_input(m, n, grid, score):
     wall_cost = int(input("Enter the cost of hitting a wall : "))
     p = int(input("Enter the number of bumpers : "))
     if p >= 0:
         for i in range(p):
             x, y, value, cost = [
-                int(x) for x in input("Enter x,y axis value and cost of bumper respectively : ").split()]
-            print(x)
-            if 1 <= x <= m and 1 <= y <= n:
+                int(i) for i in input("Enter x,y axis value and cost of bumper respectively : ").split()]
+            if 0 <= x <= m and 0 <= y <= n:
                 grid[x][y] = "B"
                 for item in grid:
                     print(item)
             else:
                 print("Please enter values of x and y axis in range...")
-                bumpers_input()
+                bumpers_input(m, n, grid, score)
+        balls_input(grid, n, y, m, x, wall_cost, score, value, cost)
     else:
         print("Please enter correct number of bumpers ")
-        bumpers_input()
+        bumpers_input(m, n, grid, score)
 
 
-def direction():
-    if dir == 0:
-        move_right(lifetime, bx, by)
-    if dir == 1:
-        move_up(lifetime, bx, by)
-    if dir == 2:
-        move_left(lifetime, bx, by)
-    if dir == 3:
-        move_down(lifetime, bx, by)
+def direction(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    if direc == 0:
+        move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+    if direc == 1:
+        move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+    if direc == 2:
+        move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+    if direc == 3:
+        move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
 
 
-def move_right(lifetime, bx, by):
-    for i in reversed(range(lifetime)):
-        if grid[bx][by] == grid[x][y]:
+def move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving right")
+        if is_wall_right(direc, by, n):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 3
+                move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper(bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
             grid[bx][by] = "B"
             grid[bx + 1][by] = 1
+            bx += 1
+            direc = 3
             for j in grid:
                 print(j)
-        else:
+            print("\n")
+            move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
             grid[bx][by] = 0
             grid[bx][by + 1] = 1
             lifetime -= 1
@@ -65,20 +73,37 @@ def move_right(lifetime, bx, by):
             for j in grid:
                 print(j)
             print("\n")
+        else:
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def move_up(lifetime, bx, by):
-    for i in reversed(range(lifetime)):
-        if grid[bx][by] == grid[x][y]:
+def move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving up")
+        if is_wall_up(direc, bx):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 0
+                move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper( bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
             grid[bx][by] = "B"
             grid[bx][by + 1] = 1
+            by += 1
+            direc = 0
             for j in grid:
                 print(j)
             print("\n")
-
-        elif is_wall(dir):
-            by = rebound_wall(lifetime, bx, by)
-        else:
+            move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
             grid[bx][by] = 0
             grid[bx - 1][by] = 1
             lifetime -= 1
@@ -86,17 +111,37 @@ def move_up(lifetime, bx, by):
             for j in grid:
                 print(j)
             print("\n")
+        else:
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def move_left(lifetime, bx, by):
-    for i in reversed(range(lifetime)):
-        if grid[bx][by] == grid[x][y]:
+def move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving left")
+        if is_wall_left(direc, by):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 1
+                move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper(bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
             grid[bx][by] = "B"
             grid[bx - 1][by] = 1
+            bx -= 1
+            direc = 1
             for j in grid:
                 print(j)
             print("\n")
-        else:
+            move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
             grid[bx][by] = 0
             grid[bx][by - 1] = 1
             lifetime -= 1
@@ -104,17 +149,37 @@ def move_left(lifetime, bx, by):
             for j in grid:
                 print(j)
             print("\n")
+        else:
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def move_down(lifetime, bx, by):
-    for i in reversed(range(lifetime)):
-        if grid[bx][by] == grid[x][y]:
+def move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving down")
+        if is_wall_down(direc, bx, m):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 2
+                move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper(bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
             grid[bx][by] = "B"
             grid[bx][by - 1] = 1
+            by -= 1
+            direc = 2
             for j in grid:
                 print(j)
             print("\n")
-        else:
+            move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
             grid[bx][by] = 0
             grid[bx + 1][by] = 1
             lifetime -= 1
@@ -122,50 +187,70 @@ def move_down(lifetime, bx, by):
             for j in grid:
                 print(j)
             print("\n")
+        else:
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def is_wall(dir):
-    while lifetime > 0:
-        if bx == 0 and dir == 1:
-            dir = 0
-            print("ball hits the wall ... ")
-            return True
-        if by == 0 and dir == 2:
-            dir = 1
-            print("ball hits the wall ... ")
-            return True
+def is_wall_right(direc, by, n):
+    if by == n-1 and direc == 0:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
 
 
-def rebound_wall(lifetime, bx, by):
-    grid[bx][by] == 0
-    print("current:", bx, by)
-    by = by + 1
-    grid[bx][by] = 1
-    print("new:", bx, by)
-    lifetime -= 1
-    for j in grid:
-        print(j)
-    print("\n")
-    return by
+def is_wall_down(direc, bx, m):
+    if bx == m-1 and direc == 3:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
 
 
-def balls_inputs():
-    global bx, by, dir, lifetime
-    bx, by, dir, lifetime = [
+def is_wall_left(direc, by):
+    if by == 0 and direc == 2:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
+
+
+def is_wall_up(direc, bx):
+    if bx == 0 and direc == 1:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
+
+
+def is_bumper(bx, by, grid, y, x):
+    if grid[bx][by] == grid[x][y]:
+        print("ball hits the bumper ... ")
+        return True
+    else:
+        return False
+
+
+def balls_input(grid, n, y, m, x, wall_cost, score, value, cost):
+    bx, by, direc, lifetime = [
         int(x) for x in input("Enter bx,by axis direction and lifetime of balls respectively : ").split()]
     bx = int(bx)
     by = int(by)
-    dir = int(dir)
-    lifetime = int(lifetime)
+    direc = int(direc)
+    if lifetime > 0:
+        lifetime = int(lifetime)
+    else:
+        print("Lifetime must be greater than zero ... ")
+        balls_input(grid, n, y, m, x, wall_cost, score, value, cost)
     grid[bx][by] = 1
-    print("The ball is placed at: \n")
+    print("The ball is placed at: ")
     for item in grid:
         print(item)
     print("\n")
-    direction()
+    direction(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
 
 
-if __name__ == '__main__':
-    grid_size()
-    bumpers_input()
-    balls_inputs()
+grid_size()
--- testfile.py
+++ testfile.py
@@ -1,118 +1,257 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
 def grid_size():
-    # try:
-        global x, y, m, n, grid
-        m, n = [int(x) for x in input("Enter values of m and n : ").split()]
-        if 2 < m < 51 and 2 < n < 51:
-            grid = [[0] * n for _ in range(m)]
-            for item in grid:
-                print(item)
-            bumpers()
+    score, total = 0, 0
+    m, n = [int(y) for y in input("Enter values of m and n : ").split()]
+    if 2 < m < 51 and 2 < n < 51:
+        grid = [[0] * n for _ in range(m)]
+        for item in grid:
+            print(item)
+        bumpers_input(m, n, grid, score)
+    else:
+        print("Values are not in range please try again ...")
+        grid_size()
+
+
+def bumpers_input(m, n, grid, score):
+    wall_cost = int(input("Enter the cost of hitting a wall : "))
+    p = int(input("Enter the number of bumpers : "))
+    if p >= 0:
+        for i in range(p):
+            x, y, value, cost = [
+                int(i) for i in input("Enter x,y axis value and cost of bumper respectively : ").split()]
+            print(i)
+            if 0 <= x <= m and 0 <= y <= n:
+                grid[x][y] = "B"
+                for item in grid:
+                    print(item)
+            else:
+                print("Please enter values of x and y axis in range...")
+                bumpers_input(m, n, grid, score)
+        balls_input(grid, n, y, m, x, wall_cost, score, value, cost)
+    else:
+        print("Please enter correct number of bumpers ")
+        bumpers_input(m, n, grid, score)
+
+
+def direction(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    if direc == 0:
+        move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+    if direc == 1:
+        move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+    if direc == 2:
+        move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+    if direc == 3:
+        move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+
+
+def move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving right")
+        if is_wall_right(direc, by, n):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 3
+                move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper(bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
+            grid[bx][by] = "B"
+            grid[bx + 1][by] = 1
+            bx += 1
+            direc = 3
+            for j in grid:
+                print(j)
+            print("\n")
+            move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
+            grid[bx][by] = 0
+            grid[bx][by + 1] = 1
+            lifetime -= 1
+            by += 1
+            for j in grid:
+                print(j)
+            print("\n")
         else:
-            print("Values are not in range please try again ...")
-            grid_size()
-    # except:
-    #     print("Values of m or n is not correct please try again ...")
-    #     grid_size()
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def bumpers():
-    # try:
-        global value, cost, x, y
-        wall_cost = int(input("Enter the cost of hitting a wall : "))
-        p = int(input("Enter the number of bumpers : "))
-        if p >= 0:
-            for i in range(p):
-                x, y, value, cost = [
-                    int(x) for x in input("Enter x,y axis value and cost of bumper respectively : ").split()]
-                if 1 <= x <= m and 1 <= y <= n:
-                    grid[x][y] = "B"
-                    for item in grid:
-                        print(item)
-                else:
-                    print("Please enter values of x and y axis in range...")
-                    bumpers()
+def move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving up")
+        if is_wall_up(direc, bx):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 0
+                move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper( bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
+            grid[bx][by] = "B"
+            grid[bx][by + 1] = 1
+            by += 1
+            direc = 0
+            for j in grid:
+                print(j)
+            print("\n")
+            move_right(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
+            grid[bx][by] = 0
+            grid[bx - 1][by] = 1
+            lifetime -= 1
+            bx -= 1
+            for j in grid:
+                print(j)
+            print("\n")
         else:
-            print("Please enter correct number of bumpers ")
-            bumpers()
-
-        balls_inputs()
-    # except:
-    #     print("Values of x and y axis or bumper is not correct please try again ... ")
-    #     bumpers()
-
-def move_right(lifetime):
-    for i in reversed(range(lifetime)):
-        step_right = grid[bx][by + 1]
-        grid[bx][by + 1] = 1
-        grid[bx][by] = 0
-        # lifetime = lifetime-1
-    for j in grid:
-        print(j)
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def move_up(lifetime):
-    for i in range(lifetime, 0):
-        step_up = grid[bx - 1][by]
-        grid[bx - 1][by] = 1
-        grid[bx][by] = 0
-        lifetime -= 1
-    for i in grid:
-        print(i)
+def move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving left")
+        if is_wall_left(direc, by):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 1
+                move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper(bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
+            grid[bx][by] = "B"
+            grid[bx - 1][by] = 1
+            bx -= 1
+            direc = 1
+            for j in grid:
+                print(j)
+            print("\n")
+            move_up(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
+            grid[bx][by] = 0
+            grid[bx][by - 1] = 1
+            lifetime -= 1
+            by -= 1
+            for j in grid:
+                print(j)
+            print("\n")
+        else:
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
-def move_left(lifetime):
-    for i in range(lifetime, 0):
-        step_left = grid[bx][by - 1]
-        grid[bx][by - 1] = 1
-        grid[bx][by] = 0
-        lifetime -= 1
-    for i in grid:
-        print(i)
+
+def move_down(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost):
+    for count in reversed(range(lifetime)):
+        print("moving down")
+        if is_wall_down(direc, bx, m):
+            score = score + wall_cost
+            lifetime = lifetime - wall_cost
+            if lifetime > 0:
+                direc = 2
+                move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+            else:
+                print("Game is over ... ")
+                break
+        elif is_bumper(bx, by, grid, y, x):
+            score = score + value
+            lifetime = lifetime - cost
+            grid[bx][by] = "B"
+            grid[bx][by - 1] = 1
+            by -= 1
+            direc = 2
+            for j in grid:
+                print(j)
+            print("\n")
+            move_left(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
+        elif lifetime > 0:
+            grid[bx][by] = 0
+            grid[bx + 1][by] = 1
+            lifetime -= 1
+            bx += 1
+            for j in grid:
+                print(j)
+            print("\n")
+        else:
+            print("Game is over ... ")
+            break
+    print("The score of ball is ", score)
+    exit()
 
 
-def move_down(lifetime):
-    for i in range(lifetime, 0):
-        grid[bx + 1][by] = 1 #next_position as 1
-        grid = grid[bx + 1][by] #next position
-        # grid[bx][by] = 0 #set current to 0
-        lifetime -= 1 #reduce lifetime
-    for i in grid:
-        print(i)
+def is_wall_right(direc, by, n):
+    if by == n-1 and direc == 0:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
 
 
-def balls_inputs():
-    global bx, by, dir, lifetime
-    bx, by, dir, lifetime = [
+def is_wall_down(direc, bx, m):
+    if bx == m-1 and direc == 3:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
+
+
+def is_wall_left(direc, by):
+    if by == 0 and direc == 2:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
+
+
+def is_wall_up(direc, bx):
+    if bx == 0 and direc == 1:
+        print("ball hits the wall ... ")
+        return True
+    else:
+        return False
+
+
+def is_bumper(bx, by, grid, y, x):
+    if grid[bx][by] == grid[x][y]:
+        print("ball hits the bumper ... ")
+        return True
+    else:
+        return False
+
+
+def balls_input(grid, n, y, m, x, wall_cost, score, value, cost):
+    bx, by, direc, lifetime = [
         int(x) for x in input("Enter bx,by axis direction and lifetime of balls respectively : ").split()]
     bx = int(bx)
     by = int(by)
-    dir = int(dir)
-    lifetime = int(lifetime)
-
+    direc = int(direc)
+    if lifetime > 0:
+        lifetime = int(lifetime)
+    else:
+        print("Lifetime must be greater than zero ... ")
+        balls_input(grid, n, y, m, x, wall_cost, score, value, cost)
     grid[bx][by] = 1
+    print("The ball is placed at: ")
     for item in grid:
         print(item)
     print("\n")
-    direction()
+    direction(lifetime, bx, by, grid, y, direc, n, m, x, wall_cost, score, value, cost)
 
 
-def direction():
-    if dir == 0:
-        move_right(0)
-    elif dir == 1:
-        move_up(1)
-    elif dir == 2:
-        move_left(2)
-    elif dir == 3:
-        move_down(3)
-    else:
-        print("You entered wrong direction ... ")
-        direction()
-
-
-if __name__ == '__main__':
-    grid_size()
+grid_size()
